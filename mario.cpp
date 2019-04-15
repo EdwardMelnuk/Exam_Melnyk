fab25394a0c17b8c1bb0ee11f9288ae70136ef6b
“Edward <edwardmelnyk1234ua@gmail.com>
2019-04-15 18:40:22 +0300

    Declared functions

--- mario.cpp
+++ mario.cpp
@@ -2,10 +2,13 @@
 #include <iomanip>
 using namespace std;
 
+	void buildStairs(int height, int choice);
+	void buildPyramide(int height, int choice2);
+	void Show(int &height, int &choice);
 
 int main()
 {
-	
+
   system("pause");
   return 0;
 }
