5c1e528095990035f8b6839575438b6f5108284b
Zhan Kozak <zhankozak@gmail.com>
2019-04-17 19:16:49 +0300

    fucs for programe

--- cash.cpp
+++ cash.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+struct Nominals
+{
+int c25 = 0;
+int c10 = 0;
+int c5 = 0;
+int c1 = 0;
+};
+
+bool TakeNumber(int number);
+bool IsValid(int number);
+int ConvertToCoins(double number);
+void ShowTrifle(int coins, Nominals &nominals);
+
 int main()
 {
 
